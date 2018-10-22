64c565912a5601e4597473578e27d6b2f441c781
ssantus <34136348+ssantus@users.noreply.github.com>
2018-10-22 17:11:50 +0200

    Adaline Neuron
    Progressed until ADALINE neuron model page 41

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -29,6 +29,7 @@ plt.legend(loc= 'upper left')
 #---------
 # Learning
 #--------
+#Perceptron
 from Perceptron import Perceptron
 
 ppn = Perceptron (eta=0.1, n_iter= 6)
@@ -41,6 +42,17 @@ plt.ylabel('Number of misclassifications')
 
 #perceptron has converged to the solution after the 6th Epochs
 
+# Perceptron Evaluation
+from Plot_decision_regions_function import plot_decision_regions
+
+plt.figure(2)
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+
+#ADALINE
 from Adaline import AdalineGD
 
 plt.figure(2)
@@ -55,18 +67,25 @@ ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
 ax[1].set_xlabel('Epochs')
 ax[1].set_ylabel('Sum-squared-error')
 ax[1].set_title('Adaline - Learning rate 0.0001')
-
 plt.show()
-#---------
-# Evaluation
-#--------
+#Adaline Evaluation
+#ADALINE after standarization of the data
+# feature scaling:standarization
 
-from Plot_decision_regions_function import plot_decision_regions
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
 
+ada = AdalineGD(n_iter=15, eta=0.01)
 plt.figure(3)
-plot_decision_regions(X, y, classifier=ppn)
-plt.xlabel('sepal length [cm]')
-plt.ylabel('petal length [cm]')
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
 plt.legend(loc='upper left')
-
+plt.figure(4)
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
 plt.show()
